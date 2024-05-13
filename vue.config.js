c0eab05c880ad21b47dc9b2c541c7cbe39172bb1
Carolina Valentini <c.valentini.a@gmail.com>
2024-05-13 12:59:14 -0400

    actualizando git ignore

--- vue.config.js
+++ vue.config.js
@@ -2,5 +2,5 @@ const { defineConfig } = require('@vue/cli-service')
 module.exports = defineConfig({
   transpileDependencies: true,
   publicPath:"/desafio-opcional/",
-  outputDir:"docs",
+  outputDir:"dist",
 })
